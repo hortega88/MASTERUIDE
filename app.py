e27d2f0f282f7522425c6710e0bdb73b7ee53b90
hortega88 <88682248+hortega88@users.noreply.github.com>
2021-08-17 21:33:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+import json
+import logging
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+#@app.route("/")
+#def hello_world():
+#    return "<p>Hello, World!</p>"
+
+@app.route("/get-price/<ticker>")
+
+def get_price(ticker):
+    
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/DIS?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    headers={'User-Agent': 'Mozilla/5.0'}
+    r = requests.get(url,headers=headers)
+    print(r.status_code)
+    result = r.json()
+
+    #print(result)
+
+    app.logger.info(result)
+
+    try:
+        price = result['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+        company_name = result['quoteSummary']['result'][0]['price']['longName']
+        exchange = result['quoteSummary']['result'][0]['price']['exchangeName']
+        currency = result['quoteSummary']['result'][0]['price']['currency']
+
+        result = {
+            "price": price,
+            "name": company_name,
+            "exchange": exchange,
+            "currency": currency
+        }
+        app.logger.info(result)
+        #print(result)
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+    except (KeyError, TypeError):
+        return Response({}, status=404, mimetype='application/json')
+    except Exception as e:
+        app.logger.error("Exception occurred", exc_info=True)
+
+
+if __name__ == '_main_':
+    app.run()
