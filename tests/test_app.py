d892828ee5f3f6316ce761383bd80e86dc28e592
hortega88 <88682248+hortega88@users.noreply.github.com>
2021-08-24 21:09:15 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,5 @@
 from app import get_price
 
-
 def test_get_price():
     dis_price = get_price('DIS').json
     print(dis_price)
@@ -9,8 +8,8 @@ def test_get_price():
     assert dis_price['name'] == 'The Walt Disney Company'
     assert dis_price['exchange'] == 'NYSE'
     assert dis_price['currency'] == 'USD'
-
     assert get_price('KSLAFSADF').status_code == 404
-
+    
+    print(get_price('KSLAFSADF').status_code)
 
 #test_get_price()
